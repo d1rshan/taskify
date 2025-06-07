d9c7484cdeaee8b24baa8b4f5c296b4274e1c0f1
darshan <darshan.paccha@gmail.com>
2025-06-07 20:45:38 +0530

    add clerk auth

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,9 @@
+const MarketingPage = () => {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      Marketing Page
+      <div className="flex flex-col justify-center items-center"></div>
+    </div>
+  );
+};
+export default MarketingPage;
