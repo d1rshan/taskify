d7476869a517f52f9dba9d8128110c2a82278a27
darshan <darshan.paccha@gmail.com>
2025-06-10 20:00:33 +0530

    add marketing/landing page

--- app/(clerk)/sign-up/[[...sign-up]]/page.tsx
+++ app/(clerk)/sign-up/[[...sign-up]]/page.tsx
@@ -1,71 +1,9 @@
-// import { useSignUp } from "@clerk/nextjs";
-// import { useRouter } from "next/router";
-// import Link from "next/link";
+import { SignUp } from "@clerk/nextjs";
 
-// export default function SignUpPage() {
-//   return <div className="flex h-full justify-center items-center"></div>;
-// }
-
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardAction,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-
-export default function CardDemo() {
+export default function SignUpPage() {
   return (
-    <Card className="w-full max-w-sm">
-      <CardHeader>
-        <CardTitle>Signup To Taskify</CardTitle>
-        <CardDescription>
-          Use your email to signup or continue with google
-        </CardDescription>
-        <CardAction>
-          <Button variant="link">Sign In</Button>
-        </CardAction>
-      </CardHeader>
-      <CardContent>
-        <form>
-          <div className="flex flex-col gap-6">
-            <div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                type="email"
-                placeholder="m@example.com"
-                required
-              />
-            </div>
-            <div className="grid gap-2">
-              <div className="flex items-center">
-                <Label htmlFor="password">Password</Label>
-              </div>
-              <Input id="password" type="password" required />
-            </div>
-            <div className="grid gap-2">
-              <div className="flex items-center">
-                <Label htmlFor="password">Enter Password Again</Label>
-              </div>
-              <Input id="password" type="password" required />
-            </div>
-          </div>
-        </form>
-      </CardContent>
-      <CardFooter className="flex-col gap-2">
-        <Button type="submit" className="w-full outline cursor-pointer">
-          Signup
-        </Button>
-        <Button variant="ghost" className="w-full">
-          Continue with Google
-        </Button>
-      </CardFooter>
-    </Card>
+    <div className="flex h-full items-center justify-center">
+      <SignUp />
+    </div>
   );
 }
